013bdcf83d4189e0cc239575336d8ce15fd52ad0
Manik Rana <manikrana54@gmail.com>
2024-03-06 15:00:59 +0530

    feat: add navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
+<script>
+	import Nav from '$lib/components/nav.svelte';
+</script>
+
 <div class="flex flex-col justify-center items-center">
-	<nav class="w-full max-w-screen-2xl border-b border-dashed border-[#525C69]"></nav>
+	<Nav />
+	<div class="flex flex-row justify-between items-center gap-1 w-full max-w-screen-2xl">
+		{#each new Array(300) as _, i}
+			<div class="w-[1px] h-[1px] bg-[#525C69] rounded-full"></div>
+		{/each}
+	</div>
 	<section class="max-w-screen-xl"></section>
 	<section class="max-w-screen-xl"></section>
 </div>
