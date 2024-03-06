4e9a2e960e4503fe2843bf19472df97c893e799c
Manik Rana <manikrana54@gmail.com>
2024-03-06 14:15:58 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-col justify-center items-center">
+	<nav class="w-full max-w-screen-2xl border-b border-dashed border-[#525C69]"></nav>
+	<section class="max-w-screen-xl"></section>
+	<section class="max-w-screen-xl"></section>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: '#FCFCFF';
+	}
+</style>
